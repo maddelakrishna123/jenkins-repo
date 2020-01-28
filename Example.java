f94b42adeac6126a347f9e9344a286e1afb3c8ea
maddelakrishna123 <maddelakrishnakumar@gmail.com>
2020-01-28 11:54:04 +0530

    second commit

--- Example.java
+++ Example.java
@@ -3,6 +3,11 @@ public class Example
 public static void main(String args[])
 {
 System.out.println("Welcome To java Programing");
+
+for(int i =0;i<=10;i++)
+{
+	System.out.println("Hi Welcome ");
+}
 }
 
 }
